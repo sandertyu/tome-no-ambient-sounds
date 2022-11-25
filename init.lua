1b265e14ff956804e4d8b1789fcb7c6d13ad0300
sandertyu <noasanders@ucdavis.edu>
2022-11-25 15:36:17 -0800

    test

--- init.lua
+++ init.lua
@@ -7,18 +7,14 @@ weight = 1000
 author = {'lowfye'}
 homepage = "https://github.com/sandertyu/tome-no-ambient-sounds"
 description = [[
-Adds an option to the UI tab in Game Options where you can choose a style to overload the "Combined Big" tactical overlay and target cursor used in the "Modern" graphic mode with those used in the "ASCII" graphic mode. The default option is "Thick", which is a properly scaled and more consistent alternative to the official ASCII borders. The "Thick" and "Thin" styles were contributed by rexorcorum.
+Removes all ambient sounds from the game. This includes removing dog barking, bird screeching, blacksmith sounds, wind, and so on. Enjoy the sweet sound of silence~
 
-I just like how it looks :)
-
-https://github.com/sandertyu/tome-ascii-borders
+https://github.com/sandertyu/tome-no-ambient-sounds
 
 ---Changelog---
-- v2.0.1 Fixed bug where borders superloaded in Map.lua would not apply until a border style was manually selected.
-- v2.0.0 Game option with 3 ASCII border style options. The border style used in v1.0.1 is now called "Official".
-- v1.0.1 Initial release.
+- v1.0.0 Initial release.
 ]]
-tags = {'ascii', 'borders', 'tactical', 'frame', 'outline'}
+tags = {'ambient', 'sound', 'sounds', 'no', 'remove', 'background', 'noise', 'dog', 'bird', 'hammer', 'last hope'}
 
 overload = false
 superload = true
